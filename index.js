ee9bb2bb18e4cfe18a0028f7dbb74b2c45f8ec5b
Jake Simmens <56897348+aerojake@users.noreply.github.com>
2021-02-07 13:37:00 -0600

    Update colors and add jReads project card (#6)
    * Save some blue color pallettes to try
    
    * Update to white background.  Add 2nd project card and links for cards.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res) => {
-    res.send("Future Portfolio site");
+    res.render("index");
 });
 
 const port = process.env.PORT || 3000;
