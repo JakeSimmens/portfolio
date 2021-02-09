0011e37eb3ca0007ce5615c8115f4a8d836333f4
Jake Simmens <rocknjake@gmail.com>
2021-02-09 15:36:29 -0600

    Add post route and name fields for contact message

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
@@ -8,8 +10,14 @@ app.get('/', (req, res) => {
     res.render("index");
 });
 
+app.post('/', (req, res) => {
+    console.log("contact post request received");
+    console.log(req.body);
+    res.redirect('/');
+})
+
 const port = process.env.PORT || 3000;
 app.server = app.listen(port, function startServer() {
     console.log("portfolio server running");
-    
+   
 });
